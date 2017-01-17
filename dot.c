33e0ad8de494e54eee5bf3700bc49c740e65dc32
Halleland <ajh5240@iastate.edu>
2017-01-17 13:08:23 -0600

    FIxed

--- dot.c
+++ dot.c
@@ -1,8 +1,13 @@
 double dot(double a[], double b[], int length) {
 	int i;
-	double sum;   
+	double sum;  
+	sum=0;
 	for (i=1; i<= length; i++) { 
-		sum = sum + a[i]*b[i];
+		sum = sum + a[i]* b[i];
+		
 		}
+		
 	return sum;
+	
 	}
+	
